8dd7c813709c7edfa825d1b781ede0e66ee639c0
Dioner D. Weiss <dionerweiss@gmail.com>
2020-03-31 09:46:54 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ const SessionController = require('./controllers/SessionController')
 const routes = express.Router()
 
 routes.get('/', function(req, res){
-   res.redirect('/incidents');
+   res.json({ msg: "bem-vindo" });
 });
 
 routes.post('/session', SessionController.create)
